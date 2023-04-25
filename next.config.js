f896118fa5cfaa0d6d8ac8834e120b3feece40ae
unknown <abubakar471>
2023-04-25 19:09:50 +0600

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  distDir: "build", //this line will tell the build to create a file with this name
+  // uncomment the below line to create a build folder when u run yarn run build command
+  // distDir: "build", //this line will tell the build to create a file with this name
 }
 
 
