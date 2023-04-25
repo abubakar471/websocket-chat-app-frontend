845cefd1ecc197cf9fa4e5438e0717cc8888703e
unknown <abubakar471>
2023-04-25 18:57:51 +0600

    initial commit

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  distDir: "build", //this line will tell the build to create a file with this name
+}
+
+
+
+module.exports = nextConfig
